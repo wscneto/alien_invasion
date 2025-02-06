3bd6eaf7f1545d5fbd630b7dd223a2f579a5cc7e
wscneto <contato.walter@proton.me>
2025-02-06 20:21:53 -0300

    added ship class

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -1,6 +1,7 @@
-import sys
 import pygame
+import game_functions as gf
 from settings import Settings
+from ship import Ship
     
 def run_game():
     # Initialize pygame, settings, and screen object
@@ -9,19 +10,16 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Set the background color
-    bg_color = (230, 230, 230)
+    # Make a ship.
+    ship = Ship(screen)
 
     # Start the main loop for the game
     while True:
-
-        # Watch for keyboard and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
 
         # Redraw the screen during each pass through the loop
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
